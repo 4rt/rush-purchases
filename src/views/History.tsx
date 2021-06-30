3e4b4c9a73f68d02a941f6e6762474c005cb6b64
artjom.sakirov <artjom.sakirov@gamesys.com>
2021-06-30 16:40:11 +0300

    fix filters, update mock data

--- src/views/History.tsx
+++ src/views/History.tsx
@@ -7,7 +7,12 @@ type InputType = 'name' | 'shopName' | 'added'
 
 const History: React.FunctionComponent = () => {
     const { history } = useSelector((state: RootState) => state);
-    const [list, filterList] = useState<Purchases[]>(history);
+    const [ list, filterList ] = useState<Purchases[]>(history);
+    const [ filters, updateFilters ] = useState<{ name: string, shopName: string, date: string }>({
+        name: '',
+        shopName: '',
+        date: ''
+    });
 
     useEffect(() => {
         filterList(history)
@@ -16,27 +21,25 @@ const History: React.FunctionComponent = () => {
     const handleChange = (e: React.SyntheticEvent): void => {
         const target = e.target as HTMLTextAreaElement;
         const value = target.value;
+        const filterName = target.getAttribute('name') as InputType;
 
-        if (!value) {
-            filterList(history)
-        } else {
-            const filterName = target.getAttribute('name') as InputType;
-
-            filterList(() => {
-                // TODO: use switch?
-                if (filterName === 'name') {
-                    return [...history].filter(purchase => (purchase.name.toLowerCase()).includes(value.toLowerCase()));
-                } else if (filterName === 'shopName') {
-                    return [...history].filter(purchase => (purchase.shopName.toLowerCase()).includes(value.toLowerCase()));
-                } else if (filterName === 'added') {
-                    // TODO: check this filter
-                    return [...history].filter(purchase => (purchase.added).includes(value));
-                } else {
-                    return [...history];
-                }
-            })
-        }
+        updateFilters(previousState => {
+            return {...previousState, [filterName]: value}
+        })
     }
+
+    useEffect(() => {
+        if (!Object.values(filters).join("")) filterList(history);
+
+        filterList(() => {
+            return [...history]
+                .filter(purchase => {
+                    return (purchase.name.toLowerCase()).includes(filters.name.toLowerCase())
+                        && (purchase.shopName.toLowerCase()).includes(filters.shopName.toLowerCase())
+                        && (purchase.added).includes(filters.date)
+                })
+        })
+    }, [filters])
 
     return (
         <main>
@@ -46,7 +49,7 @@ const History: React.FunctionComponent = () => {
                     <form onSubmit={e => e.preventDefault()}>
                         <input type='text' placeholder='Name...' name='name' onChange={ handleChange } />
                         <input type='text' placeholder='Shop name...' name='shopName' onChange={ handleChange } />
-                        <input type="text" placeholder='Added...' name='added' inputMode="numeric" pattern="[0-9]*" onChange={ handleChange } />
+                        <input type='text' placeholder='Added...' name='added' inputMode="numeric" pattern="[0-9]*" onChange={ handleChange } />
                     </form>
                 </header>
                 <Table purchases={ list } />
